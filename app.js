7e018eb18c51149f44f9ab00dd1d0ec97813943a
brochezapbrasil <brochezap@gmail.com>
2026-07-24 21:47:18 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    const botao = document.getElementById("gerarKit");
+
+    botao.addEventListener("click", () => {
+
+        const empresa = document.getElementById("empresa").value.trim();
+        const telefone = document.getElementById("telefone").value.trim();
+        const mensagem = document.getElementById("mensagem").value.trim();
+
+        if (!empresa || !telefone || !mensagem) {
+            alert("Preencha todos os campos.");
+            return;
+        }
+
+        alert("Tudo certo! Agora vamos gerar o Kit.");
+
+    });
+
+});
